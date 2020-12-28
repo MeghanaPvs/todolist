bdcce5e1bdeacb3a99b995b24c7dc00db6211d81
MeghanaPvs <pvsmeghanachowdary@gmail.com>
2020-12-28 12:31:55 +0530

    Todolist in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
-import React,{useState} from "react";
+import Typography from "@material-ui/core/Typography";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import TodoForm from "./Components/TodoForm";
+import TodoList from "./Components/TodoList";
 
-import './App.css';
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
 
 function App() {
-  useState[todos,setTodos]=useState([]);
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    // fires when app component mounts to the DOM
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    // fires when todos array gets updated
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(todo) {
+    // adds new todo to beginning of todos array
+    setTodos([todo, ...todos]);
+  }
+
+  function toggleComplete(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          };
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-     <p>React</p>
-      </header>
+      <Typography style={{ padding: 15,color: "black" }} variant="h3">
+        React Todo List
+      </Typography>
+      <TodoForm addTodo={addTodo} />
+      <TodoList
+        todos={todos}
+        removeTodo={removeTodo}
+        toggleComplete={toggleComplete}
+      />
     </div>
   );
 }
--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -1,37 +1,50 @@
+import React, { useState } from "react";
 //React and useState are react hooks
-import React,{useState} from "react";
+import { Button, TextField } from "@material-ui/core";
+const uuid = require('uuid');
 
-//keep state to keep track of input of user ; object with 3 properties 
-//This form should retun 
-//A form should consists of an input and button
-
-function Todoform(){
-
+function TodoForm({ addTodo }) {
     //keep state to keep track of input of user ; object with 3 properties: id - String,task- String and completed- boolean 
     //useState() has an array with 2 values => 1. Initial varibale 2. Function name
-    //todo is the initial variable 
-    //setTodo is a function name
-    const [todo,setTodo]=useState({
-        id:"",
-        task:"",
-        completed:false
-    });
+     //todo is the initial variable 
+   //setTodo is a function name
+    const [todo, setTodo] = useState({
+    id: "",
+    task: "",
+    completed: false
+  });
+ //Event handlers
+  function handleTaskInputChange(e) {
+    // e.target.value contains new input from onChange
+    // event for input elements
+    setTodo({ ...todo, task: e.target.value });
+  }
 
-    //Event handlers
-    function handleTaskInputChange(e)
-    {
-        setTodo({...todo, task:e.target.value});//target value : contains new input text
+  function handleSubmit(e) {
+    e.preventDefault(); // prevents browser refresh
+    // trim() gets rid of string whitespace
+    if (todo.task.trim()) {
+      addTodo({ ...todo, id: uuid.v4() });
+      setTodo({ ...todo, task: "" });
     }
-    return(
+  }
 
-        <form>
-            <input onChange={handleTaskInputChange} />            
-
-            <button/>
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <TextField
+        label="Task"
+        type="text"
+        name="task"
+        value={todo.task}
         
-        </form>
-    );
+                //  {/* 
+                //      value={todo.task} => when ever input change it is been updated
+                //      onChange: when ever input changes call handleTaskInputChange method  */}
+        onChange={handleTaskInputChange}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  );
 }
-                    {/* onChange: when ever input changes call handleTaskInputChange method  */}
 
-export default Todoform;
+export default TodoForm;
